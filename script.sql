62b6fb3e68c6a32c588a2a48028cbeed20720a97
fernando7fatec <fernando.ramos9@fatec.sp.gov.br>
2024-10-05 20:52:59 -0300

    feat: implements 20to50 and 1to100 loops

--- script.sql
+++ script.sql
@@ -74,9 +74,9 @@ $$
 DO
 $$
 DECLARE
-   numeros_0a100 INT[] := ARRAY[0];
-   numero INT;
-   evenNumber INT;
+  numeros_0a100 INT[] := ARRAY[0];
+  numero INT;
+  evenNumber INT;
 BEGIN
   -- Creates an array 
   -- filled 1to100 integers.
@@ -95,11 +95,10 @@ BEGIN
 END
 $$
 
-
-
-
+-- ****************
 -- Prints Integers
 -- btw -50 and +50
+-- ****************
 
 -- Vanilla LOOP
 DO
@@ -143,8 +142,159 @@ BEGIN
 END;
 $$
 
+-- FOR EACH LOOP
+DO
+$$
+DECLARE
+   numeros_50 INT[] := ARRAY[0];
+   numero INT;
+   evenNumber INT;
+BEGIN
+  -- Creates an array 
+  -- filled 1to100 integers.
+  FOR i IN -50..50 LOOP
+    numeros_50[i] := i;
+  END LOOP;
+   
+  -- Iterate thru. the int array
+  FOREACH numero IN ARRAY numeros_50 LOOP
+	RAISE NOTICE '%',numeros_50[numero];
+  END LOOP;
+END
+$$
 
 
+-- ****************
+-- Prints Integers
+-- btw 20 and +50
+-- ****************
+
+-- Vanilla LOOP 20to50
+DO
+$$
+DECLARE
+ counter INT := 19;
+BEGIN
+ LOOP 
+  counter = counter + 1;
+  RAISE NOTICE '%',counter;
+  EXIT WHEN counter = 50;
+ END LOOP;
+END;
+$$
+
+-- While LOOP 20to50
+DO
+$$
+DECLARE
+ counter INT := 19;
+BEGIN
+ WHILE counter <= 50 LOOP 
+  counter = counter + 1;
+  RAISE NOTICE '%',counter;
+ END LOOP;
+END;
+$$
+
+-- FOR LOOP 20to50
+DO
+$$
+DECLARE
+
+BEGIN
+	FOR i IN 20..50 LOOP
+	 RAISE NOTICE '%',i;
+	END LOOP;
+END
+$$
+
+-- FOREACH LOOP 20to50
+DO
+$$
+DECLARE
+   numeros_50 INT[] := ARRAY[0];
+   numero INT = 19;
+   evenNumber INT;
+   n INT;
+BEGIN
+  -- Creates an array 
+  FOR i IN 0..30 LOOP
+    numeros_50[i] := numero + 1;
+	numero = numero + 1;
+  END LOOP;
+   
+  -- Iterate thru. the int array
+  FOREACH n IN ARRAY numeros_50 LOOP
+	RAISE NOTICE '%',n;
+  END LOOP;
+END
+$$
+
+-- ****************
+-- Prints Integers
+-- btw 1 and 100
+-- ****************
+
+-- Vanilla LOOP 1to100
+DO
+$$
+DECLARE
+ counter INT := 0;
+BEGIN
+ LOOP 
+  counter = counter + 1;
+  RAISE NOTICE '%',counter;
+  EXIT WHEN counter = 100;
+ END LOOP;
+END;
+$$
+
+-- While LOOP 1to100
+DO
+$$
+DECLARE
+ counter INT := 0;
+BEGIN
+ WHILE counter <= 99 LOOP 
+  counter = counter + 1;
+  RAISE NOTICE '%',counter;
+ END LOOP;
+END;
+$$
+
+-- FOR LOOP 1to100
+DO
+$$
+DECLARE
+
+BEGIN
+	FOR i IN 1..100 LOOP
+	 RAISE NOTICE '%',i;
+	END LOOP;
+END
+$$
+
+-- FOREACH LOOP 1to100
+DO
+$$
+DECLARE
+   numeros_50 INT[] := ARRAY[0];
+   numero INT = 0;
+   evenNumber INT;
+   n INT;
+BEGIN
+  -- Creates an array 
+  FOR i IN 0..99 LOOP
+    numeros_50[i] := numero + 1;
+	numero = numero + 1;
+  END LOOP;
+   
+  -- Iterate thru. the int array
+  FOREACH n IN ARRAY numeros_50 LOOP
+	RAISE NOTICE '%',n;
+  END LOOP;
+END
+$$
 -- ### 1.1 ENDS ### ---
 
 
