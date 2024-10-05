7fc3a9353a07d282adee19ce62bf01d91a09923d
fernando7fatec <fernando.ramos9@fatec.sp.gov.br>
2024-10-05 19:09:08 -0300

    feat: Implements While loop 1.1

--- script.sql
+++ script.sql
@@ -1,10 +1,11 @@
-
--- ************
+-- ***********************
 -- 1.1 
 -- Resolva 
 -- https://www.beecrowd.com.br/judge/pt/problems/view/1059
 -- Prints even numbers btw 1and100
--- ************
+-- ***********************
+
+-- ### 1.1 BEGINS ### ---
 DO 
 $$ 
 DECLARE
@@ -12,12 +13,17 @@ DECLARE
  evenNumber INT := 0;
 BEGIN
  LOOP
-  SELECT MOD(counter,2) INTO evenNumber;
+  -- pick the remainder using MOD() func.
+  SELECT MOD(counter,2) INTO evenNumber; 
+  -- if the remainder is 0, then, even# it is!
   IF evenNumber = 0 THEN
    RAISE NOTICE '%',counter;
   END IF;
+  -- add 1 to counter
   counter := counter + 1;
+  -- exit when 100 is reached.
   EXIT WHEN counter = 101;
  END LOOP;
 END;
 $$
+-- ### 1.1 ENDS ### ---
