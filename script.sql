682f186fbcfae8a6b5f8606c14ca91a800d8dbf9
fernando7fatec <fernando.ramos9@fatec.sp.gov.br>
2024-10-05 19:15:01 -0300

    feat: Implements FOR Loop on 1.1

--- script.sql
+++ script.sql
@@ -6,6 +6,8 @@
 -- ***********************
 
 -- ### 1.1 BEGINS ### ---
+
+-- LOOP
 DO 
 $$ 
 DECLARE
@@ -26,4 +28,50 @@ BEGIN
  END LOOP;
 END;
 $$
+
+-- WHILE LOOP
+DO 
+$$ 
+DECLARE
+ counter INT := 0;
+ evenNumber INT := 0;
+BEGIN
+ -- WHILE LOOP
+ WHILE counter <= 100 LOOP
+  -- pick the remainder using MOD() func.
+  SELECT MOD(counter,2) INTO evenNumber; 
+  -- if the remainder is 0, then, even# it is!
+  IF evenNumber = 0 THEN
+   RAISE NOTICE '%',counter;
+  END IF;
+  -- add 1 to counter
+  counter := counter + 1;
+  -- exit when 100 is reached.
+ END LOOP;
+END;
+$$
+
+-- FOR LOOP
+DO 
+$$ 
+DECLARE
+ counter INT := 0;
+ evenNumber INT := 0;
+BEGIN
+ -- FOR LOOP
+ FOR i IN 0..100 LOOP
+  -- pick the remainder using MOD() func.
+  SELECT MOD(i,2) INTO evenNumber; 
+  -- if the remainder is 0, then, even# it is!
+  IF evenNumber = 0 THEN
+   RAISE NOTICE '%',i;
+  END IF;
+ END LOOP;
+END;
+$$
+
+
+
+
 -- ### 1.1 ENDS ### ---
+
